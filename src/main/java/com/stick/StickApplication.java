7089fd96fdcd930d88f227c426e3b3bda5d91a67
rte07542 <rte07542@gmail.com>
2026-03-09 19:58:14 +0900

    백엔드 시작

---
+++ src/main/java/com/stick/StickApplication.java
@@ -0,0 +1,13 @@
+package com.stick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stick/StickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
